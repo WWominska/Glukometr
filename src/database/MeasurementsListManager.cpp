6f91a0c73827d9d588906a9153dadd8643536b6b
Maciej Janiszewski <janiszewski_maciej@outlook.com>
2018-12-10 19:52:32 +0100

    use SQL to get data for charts

--- src/database/MeasurementsListManager.cpp
+++ src/database/MeasurementsListManager.cpp
@@ -2,7 +2,9 @@
 
 
 MeasurementsListManager::MeasurementsListManager(
-        DatabaseWorker *db, QObject *parent) : BaseListManager(db, parent) {}
+        DatabaseWorker *db, QObject *parent) : BaseListManager(db, parent) {
+    connect(this, &MeasurementsListManager::modelChanged, this, &MeasurementsListManager::getChartData);
+}
 
 QString MeasurementsListManager::getCreateQuery() const {
     return "CREATE TABLE IF NOT EXISTS measurement (" \
@@ -34,4 +36,35 @@ void MeasurementsListManager::getLastSequenceNumber(int deviceId) {
         }
         emit lastSequenceNumber(deviceId, 0);
     });
+}
+
+void MeasurementsListManager::getChartData()
+{
+    QString query = "select COUNT(smaller) as count, SUM(smaller) as "
+                    "smaller, SUM(bigger) as bigger, SUM(perfect) as "
+                    " perfect FROM (select *, value < min as 'smaller', "
+                    "value > max as 'bigger', (value >= min AND value "
+                    "<= max) as 'perfect' from measurement left join "
+                    "threshold on measurement.meal = threshold.meal)";
+    QVariantMap params;
+    if (!lastFilter.isEmpty())
+        query = QString("%1 WHERE %2").arg(query, keysToBindings(lastFilter, &params));
+
+    this->executeQuery(query,
+                       [=](QSqlQuery result) {
+        while (result.next()) {
+            if (result.value("count").toInt() == 0) {
+                m_percentageRed = 0;
+                m_percentageGreen = 0;
+                m_percentageYellow = 0;
+            } else {
+                m_percentageRed = (result.value("smaller").toFloat() / result.value("count").toInt()) * 100;
+                m_percentageGreen = (result.value("perfect").toFloat() / result.value("count").toInt()) * 100;
+                m_percentageYellow = (result.value("bigger").toFloat() / result.value("count").toInt()) * 100;
+            }
+            emit percentageRedChanged();
+            emit percentageGreenChanged();
+            emit percentageYellowChanged();
+        }
+    }, params);
 }
